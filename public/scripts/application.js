335035c62ee9e7aee960723f9ac2f376e98f14d4
Kory Nunn <knunn187@gmail.com>
2013-10-11 17:35:31 +1000

    updated the scripts to be browserified and neater

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,104 +1,82 @@
-var Members = function ($container) {
-  this._container = $container;
+var gedi = require('gedi'),
+    crel = require('crel'),
+    membersData = require('./members.json'),
+    searchExpression = require('./searchExpression.gel'),
+    membersElement;
 
-  _this = this;
-  $.get('scripts/members.json', function (data) {
-    _this._data = data;
-  });
-};
+var model = new gedi({members: membersData});
 
-Members.prototype.search = function (query) {
-  var member,
-      offices, contacts,
-      i, k;
+function updateResults(resultData){
+    var resultElements = document.createDocumentFragment();
 
-  this._container.html('');
+    for(var i = 0; i < resultData.length; i ++){
+        var member = new Member(resultData[i]);
+        resultElements.appendChild(member.renderedElement);
+    }
 
-  for (i = 0; i < this._data.length; i++) {
-    member = new Member(this._data[i]);
-    
-    if (member.matchesQuery(query)) {
-      result = crel('div', { class: 'member' },
-        crel('img', { src: member.Image, class: 'photo' }),
-        crel('div', { class: 'details' },
-          crel('h2', member.Name),
-          offices = crel('div', { class: 'offices' }),
-          contacts = crel('div', { class: 'contacts' })
+    membersElement.empty();
+    membersElement.append(resultElements);
+}
+
+function Member(data) {
+    this.data = data;
+
+    var offices,
+        contacts;
+
+    this.renderedElement = crel('div', { 'class': 'member' },
+        crel('img', { src: data.Image, 'class': 'photo' }),
+        crel('div', { 'class': 'details' },
+            crel('h2', data.Name),
+            offices = crel('div', { 'class': 'offices' }),
+            contacts = crel('div', { 'class': 'contacts' })
         )
-      );
+    );
 
-      for (k = 0; k < member.Office.length; k++) {
-        offices.appendChild(crel('p', member.Office[k]));
-      }
+    for (i = 0; i < data.Office.length; i++) {
+        offices.appendChild(crel('p', data.Office[i]));
+    }
 
-      for (k = 0; k < member.Contact.length; k++) {
-        contacts.appendChild(crel('p', 
-          crel('a', { href: member.Contact[k] }, member.Contact[k].replace(/mailto\:/g, '').replace(/http\:\/\//g, ''))
+    for (i = 0; i < data.Contact.length; i++) {
+        contacts.appendChild(crel('p',
+            crel('a', { href: data.Contact[i] }, data.Contact[i].replace(/mailto\:/g, '').replace(/http\:\/\//g, ''))
         ));
-      }
-
-      this._container.append(result);
-      $('.left-panel').removeClass('pre-display');
-      $('#loader').hide();
     }
-  }
+
+    this.renderedElement.member = this;
 }
-
-Member = function (args) {
-  for (key in args) {
-    this[key] = args[key];
-  }
-}
-
-Member.prototype.matchesQuery = function (query) {
-  var i;
-
-  query = query.toLowerCase()
-
-  if (this.Name.toLowerCase().indexOf(query) > -1) {
-    return true
-  }
-
-  for (i = 0; i < this.Office.length; i++) {
-    if (this.Office[i].toLowerCase().indexOf(query) > -1 || ((this.Office[i].toLowerCase().match(/\d\d\d\d/)[0] - parseInt(query)) < 200 && (this.Office[i].toLowerCase().match(/\d\d\d\d/)[0] - parseInt(query)) > -50) ){
-      console.log(Math.abs(this.Office[i].toLowerCase().match(/\d\d\d\d/)[0] - parseInt(query)) < 200);
-      return true;
-    }
-  }
-
-  for (i = 0; i < this.Contact.length; i++) {
-    if (this.Contact[i].toLowerCase().indexOf(query) > -1) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
 
 $(function () {
-  members = new Members($('#members'));
+    var searchForm = $('.search-form'),
+        searchBox = searchForm.find('input');
 
-  var typerTimer = null;
-  $('#search-form').keydown(function(){
-    $('#loader').show();
-    clearTimeout(typerTimer); 
-    typerTimer = setTimeout(searchSubmit, 400);
-  });
+    membersElement = $('.members');
 
-  function searchSubmit () {
-    if($('#search-form input').val() != ''){
-      $('#search-form').submit();
-    }else{
-      $('#loader').hide();
-    }
-    
-  }
+    searchForm.on('submit', function(event){
+        event.preventDefault();
+    });
 
-  $('#search-form').submit(function () {
-    var query = $('#query').val();
-    members.search(query);
+    $('.loader').hide();
 
-    return false;
-  });
+    model.bind(searchExpression, function(event){
+        var results = event.getValue();
+        updateResults(results);
+        $('.left-panel').removeClass('pre-display');
+        $('.loader').hide();
+    });
+
+    // update search term after delay
+    var typerTimer = null;
+    searchBox.on('keyup', function(event){
+        $('.loader').show();
+        clearTimeout(typerTimer);
+        typerTimer = setTimeout(function(){
+            var value = searchBox.val();
+            if(model.get('[search]') !== value){
+                model.set('[search]', value);
+            }else{
+                $('.loader').hide();
+            }
+        }, 0);
+    });
 });
